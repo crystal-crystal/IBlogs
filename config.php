0b06119927b0320c455c1c0ad34843f32a5733e5
priyanshi11081998 <priyanshipatelpre@gmail.com>
2018-12-17 13:20:50 +0530

    initialize application global variables.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+	session_start();
+
+	// connect to database
+       // coming soon...
+    $conn = mysqli_connect("localhost", "root", "", "iblog");
+
+	if (!$conn) {
+		die("Error connecting to database: " . mysqli_connect_error());
+	}
+	define ('ROOT_PATH', realpath(dirname(__FILE__)));
+	define('BASE_URL', 'http://localhost/Iblog/');
+?>
